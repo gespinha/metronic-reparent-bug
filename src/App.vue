de5f93d8c382ee46c24a271c43af84e0956bda08
Goncalo Espinha <goncaloespinha@Goncalos-MacBook-Pro-2.local>
2024-09-18 20:53:49 +0100

    initial push

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex grow">
+    <Sidebar />
+    <div class="wrapper flex grow flex-col">
+      <Header />
+      <main class="grow content pt-5" id="content" role="content">
+        <div class="container-fixed">
+
+          <div id="redbox" style="color: #fff; background: #f00;">This is the red box</div>
+
+          <ExampleMenu />
+
+        </div>
+      </main>
+      <Footer />
+    </div>
+  </div>
+  <SearchModal />
+</template>
+
+<script lang="ts" setup>
+import Sidebar from './components/Sidebar.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import SearchModal from "./components/SearchModal.vue";
+import { nextTick, onMounted } from 'vue';
+import KTComponent from './metronic/core/index';
+import KTLayout from './metronic/app/layouts/demo1.js';
+import ExampleMenu from "./components/ExampleMenu.vue";
+
+onMounted(() => {
+	nextTick(() => {
+		KTComponent.init();
+		KTLayout.init();
+	});
+});
+</script>
---
+++ src/components/ExampleMenu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dropdown" data-dropdown="true" data-dropdown-trigger="click">
+    <button type="button" class="dropdown-toggle btn btn-icon btn-clear btn-light btn-sm">
+      <i class="ki-solid ki-dots-horizontal"></i>
+    </button>
+    <div ref="actionsMenu" class="dropdown-content w-full max-w-56">
+      <div class="menu menu-default flex flex-col w-full">
+        <div class="menu-item">
+          <a class="menu-link dropdown-toggle" href="javascript:void(0)">
+            <span class="menu-icon">
+              <i class="ki-solid ki-copy"></i>
+            </span>
+            <span class="menu-title">
+              Example menu
+            </span>
+          </a>
+        </div>
+        <div class="menu-item">
+          <a class="menu-link dropdown-toggle" href="javascript:void(0)">
+            <span class="menu-icon">
+              <i class="ki-solid ki-copy"></i>
+            </span>
+            <span class="menu-title">
+              Example menu
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref, onMounted, nextTick } from 'vue'
+  import { KTReparent } from '../metronic/core/index.ts'
+
+  const actionsMenu = ref(null)
+  const actionsMenuInstance = ref(null)
+
+  onMounted(() => {
+    nextTick(() => {
+      actionsMenuInstance.value = new KTReparent(actionsMenu.value, {
+        mode: "append",
+        target: "#redbox"
+      })
+    })
+  })
+</script>
